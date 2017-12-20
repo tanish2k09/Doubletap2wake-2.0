5abb251330e2e978bced3cbb0aeaf18b02d1d889
Tanish <tanish2k09@gmail.com>
2017-12-20 13:41:00 +0530

    Revert dt2w.h to generic instead of venom-specific. Bumped dt2w to 2.0

--- include/linux/input/doubletap2wake.h
+++ include/linux/input/doubletap2wake.h
@@ -4,10 +4,6 @@
 extern int dt2w_switch;
 extern bool dt2w_scr_suspended;
 extern bool in_phone_call;
-extern unsigned int vib_strength, left, right, co_up, co_down, dt2w_distance_x, dt2w_distance_y, dt2w_time, Dt2w_regions;
-extern unsigned int vibration_strength_on_pocket_override, pocket_override_timeout, dt2w_override_taps;
-extern bool revert_area;
-//extern bool dt2w_override_vibration_to_null;
 
 void doubletap2wake_setdev(struct input_dev *);
 
